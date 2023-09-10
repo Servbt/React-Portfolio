7f6ef544ef6e4f8422b6291c8c38a25b777f3641
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-09-10 16:53:44 -0400

    added nodemailer package and got rid of form submision (temp)

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
 export default function Contact() {
+
+
+  
   return (
     <div>
       <h1>Contact Page</h1>
 
       <h2>I'd love to get in touch, please let me know if there's anything I can do for you!</h2>
 
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-
-      <button type="button" class="btn btn-primary btn-lg">Submit</button>
 
     </div>
   );
