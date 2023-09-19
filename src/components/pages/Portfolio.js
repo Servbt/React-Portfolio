7a03c066e785104c65e0e4a6aa5d06907bca06db
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-09-18 23:50:05 -0400

    added in some cards for portfolio, and some styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,22 +4,88 @@ export default function Portfolio() {
   return (
     <div>
       <div>
-        <h1>What I Know</h1>
-        <ul>
-        <li>HTML & CSS</li>
-          <li>Javascript</li>
+        <h1 className='text-center p-4 what-I-know'>What I Know</h1>
+        <div className='d-flex justify-content-center border-bottom border-2 border-dark custom-border-2'></div>
+
+        <ul className='container pt-3'>
+          <div className='row d-flex'>
+
+            <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>
+
+            <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>           
+             <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>
+          </div>
+          </ul>
+          <ul className='container pt-3'>
+          <div className='row d-flex'>
+
+            <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>
+
+            <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>           
+             <div className="card bg-transparent border-0 col" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-filetype-html" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                </svg>
+                <p className="card-text pt-3">BOTTOM TEXT</p>
+              </div>
+            </div>
+          </div>
+          </ul>
+          
+          
+
+          
+
+
+
+          {/* <li>Javascript</li>
           <li>SQL & NoSQL</li>
           <li>MongoDB</li>
           <li>React</li>
           <li>JQuery</li>
           <li>Node</li>
           <li>Bootstrap</li>
-          <li>Handlebars</li>
-        </ul>
+          <li>Handlebars</li> */}
       </div>
 
       <div>
-        <h1>Portfolio</h1>
+        {/* <h1>Portfolio</h1>
         <div>
           nexus
         </div>
@@ -28,7 +94,7 @@ export default function Portfolio() {
         </div>
         <div>
           Yumu
-        </div>
+        </div> */}
       </div>
 
     </div>
