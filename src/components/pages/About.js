8bb8a829ce0e31d2bae617015258a0dfce7b071c
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-09-25 17:43:19 -0400

    added in alot of padding and custom styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,48 +1,46 @@
 import React from 'react';
-import tron from '../../Images/ServBotTronBonne.png'
+// import tron from '../../Images/ServBotTronBonne.png'
 // import bubble from '../../Images/Icons/bg-02-free-img.png'
 
 
 
 export default function About() {
-  
-  
+
+
   return (
 
-    <div className='main-content-2'>
-      <div className='position-absolute top-50 start-0 translate-middle-y w-75 ps-3 ms-5'>
-        <h1 className='under-me-header pb-3'>About Me,</h1>
-        <h2 className='under-me-header'>I play videogames and listen to music,</h2>
-        <h5 className='under-me-header'>Making websites and looking at code is part of what I do too</h5>
-        <p className='web-devHead'> I'm looking forward to working with people to make all sorts of things! </p>
-      </div>
-
-
-      <div className='d-flex justify-content-center ms-5 ps-5 pt-5 '>
-        {/* <img className='profile-pic' src={tron} alt='profile pic'></img> */}
-        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="true">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={tron} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item">
-              <img src={tron} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item">
-              <img src={tron} className="d-block w-100" alt="..."></img>
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+    <div className='container main-content-2 pt-5 '>
+      <div className='row ps-2'>
+        <div className='col'>
+          <h1 className='about-me pb-3'>About Me</h1>
+          <div className='border-top border-4 border-dark custom-border-3 mb-3'></div>
+          <p className='under-me-header'>I play videogames and listen to music,</p>
+          <p className='under-me-header'>Making websites and looking at code is part of what I do too.</p>
+          <p className='under-me-header'> I'm looking forward to working with people to make all sorts of things! </p>
         </div>
-
       </div>
+
+      <div className='ps-3'>
+
+        <div className="diagonal-div row">
+          {/* Content inside the diagonal div */}
+          <div className='col steam-cust'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-steam" viewBox="0 0 16 16">
+              <path d="M.329 10.333A8.01 8.01 0 0 0 7.99 16C12.414 16 16 12.418 16 8s-3.586-8-8.009-8A8.006 8.006 0 0 0 0 7.468l.003.006 4.304 1.769A2.198 2.198 0 0 1 5.62 8.88l1.96-2.844-.001-.04a3.046 3.046 0 0 1 3.042-3.043 3.046 3.046 0 0 1 3.042 3.043 3.047 3.047 0 0 1-3.111 3.044l-2.804 2a2.223 2.223 0 0 1-3.075 2.11 2.217 2.217 0 0 1-1.312-1.568L.33 10.333Z" />
+              <path d="M4.868 12.683a1.715 1.715 0 0 0 1.318-3.165 1.705 1.705 0 0 0-1.263-.02l1.023.424a1.261 1.261 0 1 1-.97 2.33l-.99-.41a1.7 1.7 0 0 0 .882.84Zm3.726-6.687a2.03 2.03 0 0 0 2.027 2.029 2.03 2.03 0 0 0 2.027-2.029 2.03 2.03 0 0 0-2.027-2.027 2.03 2.03 0 0 0-2.027 2.027Zm2.03-1.527a1.524 1.524 0 1 1-.002 3.048 1.524 1.524 0 0 1 .002-3.048Z" />
+            </svg>
+            <p>My Steam</p>
+          </div>
+
+          <div className='col discord-cust'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16">
+              <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />
+            </svg>
+            <p>My Discord</p>
+          </div>
+        </div>
+      </div>
+
 
     </div>
 
