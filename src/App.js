f4ab5e8569e630a447902912901b22fc2794fd54
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-30 22:55:15 -0500

    remove orientation lock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-//  import PortfolioContainer from "./components/PortfolioContainer";
- import OrientationLock from "./components/OrientationLock";
-const App = () => <OrientationLock />;
+ import PortfolioContainer from "./components/PortfolioContainer";
+//  import OrientationLock from "./components/OrientationLock";
+const App = () => <PortfolioContainer />;
 
 export default App;
--- src/components/OrientationLock.js
+++ src/components/OrientationLock.js
@@ -1,24 +1,24 @@
-/* eslint-disable no-restricted-globals */
+// /* eslint-disable no-restricted-globals */
 
-import React, { Component } from 'react';
-import PortfolioContainer from "./PortfolioContainer";
+// import React, { Component } from 'react';
+// import PortfolioContainer from "./PortfolioContainer";
 
 
-class OrientationLock extends Component {
-  componentDidMount() {
-    // Lock the orientation to portrait mode
-    if (screen.orientation) {
-      screen.orientation.lock("portrait").catch(error => {
-        console.error('Failed to lock orientation:', error);
-      });
-    }
-  }
+// class OrientationLock extends Component {
+//   componentDidMount() {
+//     // Lock the orientation to portrait mode
+//     if (screen.orientation) {
+//       screen.orientation.lock("portrait").catch(error => {
+//         console.error('Failed to lock orientation:', error);
+//       });
+//     }
+//   }
 
-  render() {
-    return (
- <PortfolioContainer />
-    );
-  }
-}
+//   render() {
+//     return (
+//  <PortfolioContainer />
+//     );
+//   }
+// }
 
-export default OrientationLock;
+// export default OrientationLock;
