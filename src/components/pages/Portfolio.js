651fbf533291a16e355aced804a38584019ee730
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-30 23:22:16 -0500

    mobile bottom bar padding

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -35,7 +35,7 @@ export default function Portfolio() {
                 <svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' fill='currentColor' className='bi bi-hexagon-half' viewBox='0 0 16 16'>
                   <path d='M14 4.577v6.846L8 15V1l6 3.577zM8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z' />
                 </svg>
-                <p className='card-text pt-3 know-list'>Node.js</p>
+                <p className='card-text pt-3 know-list node'>Node.js</p>
               </div>
             </div>
 
@@ -58,7 +58,7 @@ export default function Portfolio() {
                   <path d='M5.062 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762V8.162h1.822c1.236 0 1.887.463 1.887 1.348 0 .896-.627 1.377-1.811 1.377H6.375z' />
                   <path d='M0 4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4zm4-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V4a3 3 0 0 0-3-3H4z' />
                 </svg>
-                <p className='card-text pt-3 know-list text-resize'>Bootstrap</p>
+                <p className='card-text pt-3 know-list text-resize boot'>Bootstrap</p>
               </div>
             </div>
 
