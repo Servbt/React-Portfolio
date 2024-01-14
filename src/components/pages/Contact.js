b259e63da55b49e4ccd6d521b627af8b23f4f514
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-01-14 17:38:30 -0500

    hover styling and cursor fix

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -93,21 +93,21 @@ export const ContactUs = () => {
           </div>
         </div>
 
-        <div className='resume col-md-4 p-5 mt-5'>
-          <div className='resume-mobile justify-content-center'>
-            <div className=' text-center resume-image p-3 rounded-5'>
-              <a href='https://drive.google.com/file/d/1HlaUy4XvOPrr71nVoHc6f0C318e-93Z9/view?usp=sharing' target='_none' className='resume'>
-                <svg xmlns='http://www.w3.org/2000/svg' width='80' height='80' fill='currentColor' className='bi bi-file-earmark-arrow-down' viewBox='0 0 16 16'>
+        <a href='https://drive.google.com/file/d/1HlaUy4XvOPrr71nVoHc6f0C318e-93Z9/view?usp=sharing' target='_none' className='resume col-md-4 p-5 mt-5 link-bg'>
+          <div className='resume-mobile justify-content-center link-bg'>
+            <div className=' text-center resume-image p-3 rounded-5 link-bg'>
+              <div className='resume'>
+                <svg xmlns='http://www.w3.org/2000/svg' width='80' height='80' fill='currentColor' className='bi bi-file-earmark-arrow-down link-bg' viewBox='0 0 16 16'>
                   <path d='M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z' />
                   <path d='M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z' />
                 </svg>
-              </a>
-              <a href='https://drive.google.com/file/d/1HlaUy4XvOPrr71nVoHc6f0C318e-93Z9/view?usp=sharing' target='_none' className='resume'>
-                <h3 className=' pt-3'>
+              </div>
+              <div className='resume'>
+                <h3 className='pt-3 link-bg'>
                   My Resume
                 </h3>
-              </a>
-              <a className='resume' href='https://drive.google.com/file/d/1HlaUy4XvOPrr71nVoHc6f0C318e-93Z9/view?usp=sharing' target='_none'>(It's a Google Drive link)</a>
+              <p className='link-bg'>(It's a Google Drive link)</p>
+              </div>
             </div>
           </div>
           {/* <div className='  icon-container'>
@@ -127,7 +127,7 @@ export const ContactUs = () => {
               </svg>
             </a>
           </div> */}
-        </div>
+        </a>
       </div>
 
 
